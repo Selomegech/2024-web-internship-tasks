03e2cfadb6b5a94b7cb7bf8729c554ba8f2a074d
selome <selomegech@gmail.com>
2024-08-07 09:10:03 +0300

    new-commit

--- task-8/app/page.tsx
+++ task-8/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import LoginForm from "@/app/UI/Loginform";
+
+import LoginForm from "@/app/UI/LoginForm";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    
       <LoginForm />
 
-    </main>
+    
   );
 }
